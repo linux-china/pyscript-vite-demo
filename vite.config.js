458950fa0e6add52cc7be9ea1d1e907f6e175a7a
linux_china <libing.chen@gmail.com>
2022-05-05 14:25:41 -0700

    feat: ignore js and pyodide modules

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        watch: {
+            ignored: ['js/', "pyodide/"]
+        }
+    },
     plugins: [
         pyscriptHmr()
     ]
